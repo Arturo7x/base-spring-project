b31222a8faefadc96eed40033ed27ae5e4c1d0f1
arturo <arturo7x@hotmail.com>
2020-03-27 13:44:01 -0600

    first commit

---
+++ src/main/java/com/ncapas/ncapas/HelloController.java
@@ -0,0 +1,14 @@
+package com.ncapas.ncapas;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Spring Boot Example!!";
+    }
+
+}
---
+++ src/main/java/com/ncapas/ncapas/NcapasApplication.java
@@ -0,0 +1,19 @@
+package com.ncapas.ncapas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+// @ComponentScan(basePackages = {"com.ncapas"})
+public class NcapasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NcapasApplication.class, args);
+    }
+
+}
+
+
